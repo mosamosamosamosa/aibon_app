30e81b1d1fa5cc6deaa4c11e008d859fea6d72ab
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-24 17:03:45 +0900

    ✨画面デザイン

---
+++ lib/constans.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kFontColor = Color(0xFF666666);
+const kBackgroundColor = Color(0xFFBCE2E8);
+const kButtonColor = Color(0xFFFFEFEF);
+const kAppBarColor = Color(0xFFFFFAE3);
