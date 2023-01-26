e5057cb2776c04cfaf5ef9af63842b6bda1d3f8e
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-01-26 13:06:54 +0900

    ✨カラーコード追加

--- lib/constans.dart
+++ lib/constans.dart
@@ -5,3 +5,6 @@ const kFontColor = Color(0xFF666666);
 const kBackgroundColor = Color(0xFFBCE2E8);
 const kButtonColor = Color(0xFFFFEFEF);
 const kAppBarColor = Color(0xFFFFFAE3);
+
+const kCanselColor = Color(0XFFE3EEF9);
+const kEndColor = Color(0XFF90D7EC);
